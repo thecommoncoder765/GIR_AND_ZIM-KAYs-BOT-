3256b47f435e68ec8ad5cdcce28cb337d6aed237
Elijah Sears <elijahsears7@gmail.com>
2020-07-08 20:42:13 -0500

    Add src folder.

---
+++ src/main/javascript/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+bot.on('ready', () => {
+    console.log("ZIM AND GIR ONLINE!!!!!!!!!");
+})
