a9981949943879c144435a96d627802543746e53
Elijah Sears <elijahsears7@gmail.com>
2020-07-08 20:41:17 -0500

    Modify file structure.

--- src/main/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-
-
-bot.on('ready', () => {
-    console.log("ZIM AND GIR ONLINE!!!!!!!!!");
-})
