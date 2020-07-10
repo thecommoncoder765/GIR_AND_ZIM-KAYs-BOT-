355172692103f0d15a513fc19d2f7aff686fcd3a
Elijah Sears <elijahsears7@gmail.com>
2020-07-10 11:48:05 -0500

    Test a new function.

---
+++ src/Bot/main/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+bot.on('ready', () => {
+    console.log("ZIM AND GIR ONLINE!!!!!!!!!");
+})
+
+
+bot.on('message', msg => {
+    let args = msg.content.substring(process.env.PREFIX.length).split(" ");
+
+    switch (args[0]) {
+        case 'hey-gir':
+            if (!args[1]) {
+                msg.reply("what, you rat?");
+            }
+
+            if (args[0] === 'am-i-stupid?') {
+                msg.reply('yes in fact, you are.');
+            }
+    }
+})
+
+
+bot.login(process.env.TOKEN);
